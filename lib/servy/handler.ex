4c070407618e427a1ce2390b4d16bc899d8b1982
maxkrieg <maxwell.krieg@gmail.com>
2022-01-11 15:44:12 -0700

    reorder funcs, add emojifier

--- lib/servy/handler.ex
+++ lib/servy/handler.ex
@@ -2,12 +2,14 @@ defmodule Servy.Handler do
   def handle(request) do
     request
     |> parse()
-    |> log
+    |> rewrite_path()
+    |> rewrite_params()
+    |> log()
     |> route()
+    |> track()
+    |> emojify()
     |> format_response()
   end
-
-  def log(conv), do: IO.inspect(conv, label: "Request")
 
   def parse(request) do
     [method, path, _] =
@@ -22,6 +24,20 @@ defmodule Servy.Handler do
       status: nil
     }
   end
+
+  def rewrite_path(%{ path: "/wildlife"} = conv) do
+    %{ conv | path: "/wildthings"}
+  end
+
+  def rewrite_path(conv), do: conv
+
+  def rewrite_params(%{ path: "/bears?id=" <> id } = conv) do
+    %{conv | path: "/bears/#{id}" }
+  end
+
+  def rewrite_params(conv), do: conv
+
+  def log(conv), do: IO.inspect(conv, label: "Request")
 
   def route(%{ path: "/wildthings", method: "GET" } = conv) do
     %{ conv | status: 200, resp_body: "Bears, Lions, and Tigers"}
@@ -42,6 +58,21 @@ defmodule Servy.Handler do
   def route(%{ path: path } = conv) do
     %{ conv | status: 404, resp_body: "No #{path} here" }
   end
+
+  def track(%{status: 404, path: path} = conv) do
+    IO.puts("Warning: #{path} does not exist")
+    conv
+  end
+
+  def track(conv), do: conv
+
+  def emojify(%{ status: 200, resp_body: resp_body } = conv) do
+    emojies = String.duplicate("😃", 5)
+    resp_body = emojies <> "\n" <> resp_body <> "\n" <> emojies
+    %{ conv | resp_body: resp_body}
+  end
+
+  def emojify(conv), do: conv
 
   def format_response(%{ resp_body: resp_body, status: status }) do
     """
@@ -106,11 +137,29 @@ Accept: */*
 
 """
 
+request6 = """
+GET /wildlife HTTP/1.1
+Host: example.com
+User-Agent: ExampleBrowser/1.0
+Accept: */*
+
+"""
+
+request7 = """
+GET /bears?id=1 HTTP/1.1
+Host: example.com
+User-Agent: ExampleBrowser/1.0
+Accept: */*
+
+"""
+
 response = Servy.Handler.handle(request)
 response2 = Servy.Handler.handle(request2)
 response3 = Servy.Handler.handle(request3)
 response4 = Servy.Handler.handle(request4)
 response5 = Servy.Handler.handle(request5)
+response6 = Servy.Handler.handle(request6)
+response7 = Servy.Handler.handle(request7)
 
 IO.puts("------------------")
 IO.puts(response)
@@ -122,3 +171,7 @@ IO.puts("------------------")
 IO.puts(response4)
 IO.puts("------------------")
 IO.puts(response5)
+IO.puts("------------------")
+IO.puts(response6)
+IO.puts("------------------")
+IO.puts(response7)
